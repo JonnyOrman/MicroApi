41564af27b2338ae8a25ae69ba977d43e4c36056
Jonathan Orman <jonathanpcorman@gmail.com>
2022-02-11 20:04:53 +0000

    MicroApi.1.0.0-alpha.5 - Make IMicroApiBuilder more compatible with other packages

--- MicroApi/IMicroApiBuilder.cs
+++ MicroApi/IMicroApiBuilder.cs
@@ -2,19 +2,11 @@
 
 namespace MicroApi;
 
-public interface IMicroApiBuilder<T, TParameters>
+public interface IMicroApiBuilder<TParameters, TBuilder>
 {
-    IMicroApiBuilder<T, TParameters> Where<TProperty>(Expression<Func<TParameters, TProperty>> propertyExpression);
-
-    IMicroApiBuilder<T, TParameters> IsRequired();
-
     void Start();
 
-    IMicroApiBuilder<T, TParameters> MustPass<TValidationRule>()
-        where TValidationRule : IValidationRule<TParameters>, new();
+    TBuilder Where<TProperty>(Expression<Func<TParameters, TProperty>> propertyExpression);
 
-    IMicroApiBuilder<T, TParameters> OnSuccess<TSuccessEvent>()
-        where TSuccessEvent : class, IOperationSuccessEvent<T, TParameters>;
-    IMicroApiBuilder<T, TParameters> OnFailure<TFailedEvent>()
-        where TFailedEvent : class, IOperationFailedEvent<TParameters>;
+    TBuilder IsRequired();
 }
