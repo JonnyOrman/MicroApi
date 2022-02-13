50d557a495b994252087e725abe5d081b1055a75
Jonathan Orman <jonathanpcorman@gmail.com>
2022-02-13 19:35:22 +0000

    Fix MicroApi.Create.Example/Program.cs

--- Create/MicroApi.Create.Example/Program.cs
+++ Create/MicroApi.Create.Example/Program.cs
@@ -1,6 +1,12 @@
 using MicroApi.Create;
-using UserCreateApi;
+using MicroApi.Create.Example;
+using Microsoft.Extensions.DependencyInjection;
 
-MicroCreateApi.New<User, int, UserParameters, UserCreator>(args)
+MicroCreateApi.New<User, int, UserParameters, UserCreator>(args, serviceCollection =>
+    {
+        serviceCollection.AddSingleton<IUserCreateMessageGenerator, UserCreateMessageGenerator>();
+    })
     .Where(parameters => parameters.Name).IsRequired()
+    .MustPass<EmailRule>()
+    .OnSuccess<LogUserCreateSuccessEvent>()
     .Start();
